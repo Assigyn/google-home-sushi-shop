7571ce5917282b9abc98feb8d285880cc9c92e66
Assigyn <anne.sophie.lesaint@gmail.com>
2017-12-13 00:38:31 +0100

    new version of index.php

--- index.php
+++ index.php
@@ -25,8 +25,8 @@ if ($method == "POST") {
     }
 
     $response = new \stdClass();
-    $response->speech = "";
-    $response->displayText = "";
+    $response->speech = $speech;
+    $response->displayText = $speech;
     $response->source = "webhook";
     echo json_encode($response);
 
