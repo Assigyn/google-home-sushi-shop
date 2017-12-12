3cb00a367ed09534046a96d0f4bd62e195d84699
Assigyn <anne.sophie.lesaint@gmail.com>
2017-12-13 00:50:13 +0100

    new version of index.php with questionOne

--- index.php
+++ index.php
@@ -8,19 +8,32 @@ if ($method == "POST") {
     $json = json_decode($requestBody);
 
     $difficulty = $json->result->parameters->difficulty;
+    $questionOne = $json->result->parameters->questionOne;
 
     switch ($difficulty){
         case 'médium':
             $speech = "L'équilibre est toujours une bonne chose";
             break;
         case 'débutant':
-            $speech = "Vous avez raison, mieux vaut commencer léger";
+            $speech = "Vous avez raison, mieux vaut commencer léger : commençons par un petit quiz.
+            Première question : quelle est la capitale actuelle du Japon ?";
             break;
         case 'difficile':
             $speech = "Vous avez de l'audace, bravo !";
             break;
         default:
             $speech = "Désolé, je n'ai pas saisi le niveau de difficulté ?";
+            break;
+    }
+
+    switch($questionOne){
+        case 'Tokyo':
+            $speech = "Bonne réponse ! Tokyo est aussi la plus grande ville
+            du Japon. Son agglomération est peuplée de 42 millions d'habitants.";
+            break;
+        default:
+            $speech = "Désolé, ce n'est pas la bonne réponse. Un indice : cette
+            ville est une des plus densément peulée du monde.";
             break;
     }
 
