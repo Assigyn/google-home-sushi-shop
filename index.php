58a9d8a43e62280c7e2e3c9d24530ce80a2157ac
Assigyn <anne.sophie.lesaint@gmail.com>
2017-12-13 10:49:00 +0100

    Test with new questions

--- index.php
+++ index.php
@@ -52,11 +52,11 @@ if ($method == "POST") {
 
     if(isset($questionTwo)){
         switch($questionTwo){
-            case 'Kyoto' :
+            case 'Tokyo' :
                 $speech = "Bonne réponse ! Kyoto possède un temple immense : le pavillon d'or.'
                            Nous arrivons enfin à la dernière question ! Quelle ville était capitale avant Tokyo ?";
                 break;
-            case 'kyoto' :
+            case 'tokyo' :
                 $speech = "Bonne réponse ! Kyoto possède un temple immense : le pavillon d'or.'
                            Nous arrivons enfin à la dernière question ! Quelle ville était capitale avant Tokyo ?";
                 break;
